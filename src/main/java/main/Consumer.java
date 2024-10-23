11421a8db9c141521ad334c574472f21cef5399e
Jake Thurgood <jkathurgood@gmail.com>
2024-10-22 20:42:11 -0600

    Request keys

--- src/main/java/main/Consumer.java
+++ src/main/java/main/Consumer.java
@@ -4,24 +4,27 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Consumer {
 
+    private static String REQUEST_BUCKET = "usu-cs5250-hobbes-requests";
     private static String STORAGE_BUCKET = "usu-cs5250-hobbes-web";
     private static ObjectMapper mapper = new ObjectMapper();
     public static void main(String[] args) {
-        System.out.println("Testing Object mapping and put");
-        Widget widget = new Widget("WidgetCreateRequest", "123", "widget-456", "Kellen Moore");
+
         S3Client s3 = S3Client.builder()
         .region(Region.US_EAST_1)
         .credentialsProvider(ProfileCredentialsProvider.create())
         .build();
 
-        putWidgetS3Bucket(s3, widget);
+        checkForRequests(s3);
 
         System.exit(0);
     }
@@ -40,5 +43,22 @@ public class Consumer {
         }
         
         System.out.println("JSON object uploaded successfully!");
+    }
+
+    public static String checkForRequests(S3Client s3Client){
+        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder()
+            .bucket(REQUEST_BUCKET)
+            .maxKeys(1)
+            .build();
+
+        ListObjectsV2Response listResponse = s3Client.listObjectsV2(listObjectsRequest);
+        if (!listResponse.contents().isEmpty()) {
+            S3Object firstObject = listResponse.contents().get(0);
+            System.out.println("First Key: " + firstObject.key());
+            return firstObject.key();
+        } else {
+            System.out.println("The bucket is empty.");
+        }
+        return null;
     }
 }
