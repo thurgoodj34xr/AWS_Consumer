baf8828819da8e03621408b37e00154f5c351ac4
Jake Thurgood <jkathurgood@gmail.com>
2024-10-23 19:55:01 -0600

    Looping

--- src/main/java/main/Consumer.java
+++ src/main/java/main/Consumer.java
@@ -4,10 +4,12 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,6 +17,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 public class Consumer {
 
@@ -28,13 +31,23 @@ public class Consumer {
         .credentialsProvider(ProfileCredentialsProvider.create())
         .build();
 
-        String key = checkForRequests(s3);
-        requestKeyWidget(s3, key);
-
-        System.exit(0);
+        while (true) {
+            String key = checkForRequests(s3);
+            
+            if (key.isEmpty()) {
+                sleepFor100Ms();
+                continue;
+            }
+    
+            Widget widget = requestKeyWidget(s3, key);
+            
+            if (putWidgetS3Bucket(s3, widget)) {
+                deleteKeyInS3(s3, key);
+            }
     }
+}
 
-    public static void putWidgetS3Bucket(S3Client s3Client, Widget widget){
+    public static boolean putWidgetS3Bucket(S3Client s3Client, Widget widget){
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
             .bucket(STORAGE_BUCKET)
             .key(widget.getKey())
@@ -43,11 +56,12 @@ public class Consumer {
         // Upload JSON object to S3 as a string
         try{
             s3Client.putObject(putObjectRequest, RequestBody.fromString(mapper.writeValueAsString(widget)));
+            System.out.println("JSON object uploaded successfully!");
+            return true;
         }catch (JsonProcessingException e){
             System.out.println("JSON Object couldn't be converted");                
         }
-        
-        System.out.println("JSON object uploaded successfully!");
+        return false;
     }
 
     public static String checkForRequests(S3Client s3Client){
@@ -93,5 +107,31 @@ public class Consumer {
                 System.out.println("Error retrieving or parsing the object: " + e.getMessage());
             }
             return null;
+    }
+
+    public static boolean deleteKeyInS3(S3Client s3Client, String key){
+                try {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(REQUEST_BUCKET)
+                    .key(key)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+            // Succeeds if no throw
+            System.out.println("Object deleted successfully!");
+            return true;
+        } catch (S3Exception e) {
+            System.err.println("Error deleting object: " + e.getMessage());
+        }
+        return false;
+    }
+
+    private static void sleepFor100Ms() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Sleep interrupted: " + e.getMessage());
+        }
     }
 }
